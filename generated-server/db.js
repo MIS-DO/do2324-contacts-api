b14231d6f4e1aaae7c4e445ab4b3dcfcfb117c30
josemgarcia <josemgarcia@us.es>
2024-01-25 19:39:09 +0100

    Change nedb to mongodb

--- generated-server/db.js
+++ generated-server/db.js
@@ -1,11 +1,17 @@
 'use strict';
 
-var Datastore = require('nedb');
+var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 var path = require('path');
 
-const dbPath = 'data/';
-const dbFileName = path.join(__dirname, dbPath, 'contacts.db');
+// Connection URL
+const url = 'mongodb://172.17.0.1:27017';
+
+// Database Name
+const dbName = 'contacts';
+
+// Create a new MongoClient
+const client = new MongoClient(url);
 
 var _db;
 
@@ -15,11 +21,15 @@ module.exports.connect = function connect(cb) {
         console.warn("Trying to create the DB connection again!");
         return cb(null, _db);
     }
-    _db = new Datastore({
-        filename: dbFileName,
-        autoload: true
+    client.connect(function (err) {
+        if (err) {
+            console.error("Error connecting to DB!", err);
+            process.exit(1);
+        }
+        _db = client.db(dbName).collection(dbName);
+        return cb(null, _db);
     });
-    return cb(null, _db);
+
 };
 
 //Return the connection to the database if it was previously created
@@ -57,7 +67,7 @@ module.exports.init = function init() {
 
 //Executes the query and return the result in the callback function
 module.exports.find = function find(query, cb) {
-    return this.getConnection().find(query, cb);
+    return this.getConnection().find(query).toArray(cb);
 };
 
 //Inserts a new document in the database
@@ -72,5 +82,7 @@ module.exports.update = function update(query, newDoc, cb) {
 
 //Removes a document from the database
 module.exports.remove = function remove(query, cb) {
-    return this.getConnection().remove(query, cb);
+    return this.getConnection().remove(query, function (err, res) {
+        cb(err, res.result.n);
+    });
 };
